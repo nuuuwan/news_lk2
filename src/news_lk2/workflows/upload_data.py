6649fa20152bd76d1df2488a73eac0846d7bb2c0
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-23 16:04:39 +0530

    Added translate logic into upload

--- src/news_lk2/workflows/upload_data.py
+++ src/news_lk2/workflows/upload_data.py
@@ -1,4 +1,5 @@
 from news_lk2._utils import log
+from news_lk2.core import TranslatedArticle
 from news_lk2.core.filesys import git_checkout
 from news_lk2.custom_newspapers import newspaper_class_list
 from news_lk2.workflows import common
@@ -12,7 +13,13 @@ def main(is_test_mode=False):
     git_checkout()
     for newspaper_class in newspaper_class_list:
         log.debug(f'Scraping {newspaper_class.__name__}...')
-        newspaper_class.scrape()
+        article_list = newspaper_class.scrape()
+
+        log.debug(f'Translating articles for {newspaper_class.__name__}...')
+        for article in article_list:
+            translated_article = TranslatedArticle.initFromArticle(article)
+            translated_article.store()
+
         if is_test_mode:
             break
     common.build_readme_summary()
