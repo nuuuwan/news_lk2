d00c9aafa47816076d62937f14efa658f817c4a3
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-23 12:45:19 +0530

    More logging

--- src/news_lk2/upload_data.py
+++ src/news_lk2/upload_data.py
@@ -69,6 +69,7 @@ def main(is_test_mode=False):
     log.debug(f'{is_test_mode=}')
     git_checkout()
     for newspaper_class in newspaper_class_list:
+        log.debug(f'Scraping {newspaper_class.__name__}...')
         newspaper_class.scrape()
         if is_test_mode:
             break
