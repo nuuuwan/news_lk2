cc0c40ddb776f83f8edef60a8c500558d86655fa
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-23 08:40:46 +0530

    Added DBSJeyarajCom

--- src/news_lk2/custom_newspapers/__init__.py
+++ src/news_lk2/custom_newspapers/__init__.py
@@ -2,16 +2,18 @@ from news_lk2.custom_newspapers.AdaDeranaLk import AdaDeranaLk
 from news_lk2.custom_newspapers.DailyFtLk import DailyFtLk
 from news_lk2.custom_newspapers.DailyMirrorLk import DailyMirrorLk
 from news_lk2.custom_newspapers.DailyNewsLk import DailyNewsLk
+from news_lk2.custom_newspapers.DBSJeyarajCom import DBSJeyarajCom
 from news_lk2.custom_newspapers.EconomyNextCom import EconomyNextCom
 from news_lk2.custom_newspapers.IslandLk import IslandLk
 from news_lk2.custom_newspapers.NewsFirstLk import NewsFirstLk
 
 newspaper_class_list = [
     AdaDeranaLk,
-    EconomyNextCom,
     DailyFtLk,
     DailyMirrorLk,
     DailyNewsLk,
+    DBSJeyarajCom,
+    EconomyNextCom,
     IslandLk,
     NewsFirstLk,
 ]
