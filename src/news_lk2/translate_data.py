1bcb16e92bc4b7f3fbd12fd353fd1f41455544aa
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-23 09:25:08 +0530

    Added main and is_test_mode

--- src/news_lk2/translate_data.py
+++ src/news_lk2/translate_data.py
@@ -2,20 +2,24 @@ from news_lk2.analysis.paper import get_articles
 from news_lk2.core import TranslatedArticle
 from news_lk2.core.filesys import git_checkout
 
-MAX_ARTICLES_TO_TRANSLATE = 30
+MAX_ARTICLES_TO_TRANSLATE = 20
 
 
-def main():
+def main(is_test_mode=False):
     git_checkout()
     articles = get_articles()
     i_translated = 0
     for article in articles:
         translated_article = TranslatedArticle.initFromArticle(article)
-        if translated_article.store():
-            i_translated += 1
+        if not translated_article.store():
+            continue
+
+        i_translated += 1
         if i_translated >= MAX_ARTICLES_TO_TRANSLATE:
+            break
+        if is_test_mode:
             break
 
 
 if __name__ == '__main__':
-    main()
+    main(is_test_mode=False)
