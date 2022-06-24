6fb33c12250eca05b32dfd50f59b5e5632dc5bf6
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-24 08:47:22 +0530

    Use time_window

--- src/news_lk2/workflows/test_backpopulate.py
+++ src/news_lk2/workflows/test_backpopulate.py
@@ -1,4 +1,9 @@
+from utils import timex
+
 from news_lk2.workflows import backpopulate
 
 if __name__ == '__main__':
-    backpopulate.main(is_test_mode=True)
+    backpopulate.main(
+        time_window=timex.SECONDS_IN.DAY,
+        is_test_mode=True,
+    )
