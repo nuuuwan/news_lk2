239ca177c0df8ccff35b4659b7be9d36b9bbae9a
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-24 07:57:38 +0530

    Process file by file

--- src/news_lk2/workflows/backpopulate.py
+++ src/news_lk2/workflows/backpopulate.py
@@ -1,5 +1,6 @@
 from news_lk2._utils import log
-from news_lk2.analysis.paper import get_articles
+from news_lk2.analysis import paper
+from news_lk2.core import Article
 from news_lk2.core.filesys import git_checkout
 
 MAX_ARTICLES_TO_TRANSLATE = 40
@@ -9,15 +10,19 @@ def main(is_test_mode=False):
     log.debug(f'{is_test_mode=}')
 
     git_checkout(force=not is_test_mode)
-    articles = get_articles()
-    n_articles = len(articles)
-    log.debug(f'Backpopulating on {n_articles} articles...')
+    article_files = paper.get_article_files()
+    if is_test_mode:
+        article_files = article_files[:10]
 
-    for article in articles:
-        log.debug(f'(Re)Storing {article.url}')
+    n = len(article_files)
+    log.debug(f'Backpopulating on {n} articles...')
+
+    for i, article_file in enumerate(article_files):
+        article = Article.load_from_file(article_file)
+        log.info(f'{i +  1}/{n} {article.url} done.')
         article.store()
 
-    log.debug(f'Backpopulated {n_articles} articles.')
+    log.debug(f'Backpopulated {n} articles.')
 
 
 if __name__ == '__main__':
