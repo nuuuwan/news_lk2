994cffa3811a252865a91ed45eb08c3b5b505acd
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-25 17:43:37 +0530

    Added fuzzy grouping

--- src/news_lk2/workflows/common.py
+++ src/news_lk2/workflows/common.py
@@ -1,5 +1,6 @@
 import os
 
+from fuzzywuzzy import fuzz
 from utils import File, JSONFile, TSVFile, timex
 
 from news_lk2._utils import log
@@ -13,6 +14,7 @@ GITHUB_BASE = 'https://github.com/nuuuwan/news_lk2/blob/data'
 TMP_BASE = '/tmp/news_lk2'
 DELIM = ':'
 MAX_ARTICLE_AGE_FOR_TRENDS = timex.SECONDS_IN.WEEK
+MIN_FUZZ_RATIO_FOR_GROUP = 85
 
 
 def build_trending_summary():
@@ -41,23 +43,58 @@ def build_trending_summary():
     for ent in ents:
         if ent['label'] not in THING_ENTS:
             continue
-        k = ent['text'] + DELIM + ent['label']
+        k = ent['text']
 
         if k not in ent_text_to_n:
             ent_text_to_n[k] = 0
         ent_text_to_n[k] += 1
 
+    sorted_ent_and_n = sorted(
+        ent_text_to_n.items(),
+        key=lambda x: -x[1],
+    )
+
+    sorted_ents = list(
+        map(
+            lambda x: x[0],
+            sorted_ent_and_n,
+        )
+    )
+
+    ent_to_group = {}
+    n = len(sorted_ents)
+    for i in range(0, n):
+        if sorted_ents[i] in ent_to_group:
+            continue
+        for j in range(0, i + 1):
+            if (
+                fuzz.ratio(sorted_ents[i], sorted_ents[j])
+                > MIN_FUZZ_RATIO_FOR_GROUP
+            ):
+                ent_to_group[sorted_ents[i]] = sorted_ents[j]
+                if i != j:
+                    log.debug(f'{sorted_ents[i]} -> {sorted_ents[j]}')
+                break
+
+    group_to_n = {}
+    for ent, n in sorted_ent_and_n:
+        group = ent_to_group[ent]
+        if group not in group_to_n:
+            group_to_n[group] = 0
+        group_to_n[group] += n
+
+    sorted_group_and_n = sorted(
+        group_to_n.items(),
+        key=lambda x: -x[1],
+    )
+
     data_list = list(
         map(
             lambda x: dict(
-                ent_text=x[0].split(DELIM)[0],
-                ent_label=x[0].split(DELIM)[1],
+                ent_group=x[0],
                 n=x[1],
             ),
-            sorted(
-                ent_text_to_n.items(),
-                key=lambda x: -x[1],
-            ),
+            sorted_group_and_n,
         )
     )
 
@@ -168,6 +205,6 @@ def build_articles_summary():
 
 
 def build_all_summaries():
+    build_trending_summary()
     build_articles_summary()
     build_readme_summary()
-    build_trending_summary()
