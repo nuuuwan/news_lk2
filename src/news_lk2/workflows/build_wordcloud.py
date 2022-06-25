9438261c104627168d2e057ed9449ee9f45cb780
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-25 18:24:04 +0530

    Saved legacy file

--- src/news_lk2/workflows/build_wordcloud.py
+++ src/news_lk2/workflows/build_wordcloud.py
@@ -1,8 +1,9 @@
 import os
 import random
+import shutil
 
 import matplotlib.pyplot as plt
-from utils import dt
+from utils import dt, timex
 from wordcloud import STOPWORDS, WordCloud
 
 from news_lk2._utils import log
@@ -35,3 +36,14 @@ def build_wordcloud(group_to_n):
     wordcloud_file = os.path.join(DIR_REPO, 'wordcloud.png')
     plt.savefig(wordcloud_file)
     log.info(f'Saved wordcloud to {wordcloud_file}')
+
+    DIR_WORDCLOUDS = os.path.join(DIR_REPO, 'wordclouds')
+    if not os.path.exists(DIR_WORDCLOUDS):
+        os.mkdir(DIR_WORDCLOUDS)
+
+    time_id = timex.get_time_id(timezone=timex.TIMEZONE_OFFSET_LK)
+    history_wordcloud_file = os.path.join(
+        DIR_WORDCLOUDS, f'wordcloud-{time_id}.png'
+    )
+    shutil.copyfile(wordcloud_file, history_wordcloud_file)
+    log.info(f'Saved wordcloud to {history_wordcloud_file}')
