6afc10b46b10448c22620de00f798277ad218c1d
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-26 20:27:12 +0530

    Updated wordcloud params

--- src/news_lk2/workflows/build_wordcloud.py
+++ src/news_lk2/workflows/build_wordcloud.py
@@ -1,32 +1,21 @@
 import os
-import random
 import shutil
 
 import matplotlib.pyplot as plt
-from utils import dt, timex
-from wordcloud import STOPWORDS, WordCloud
+from utils import timex
 
 from news_lk2._utils import log
 from news_lk2.core.filesys import DIR_REPO
+from wordcloud import WordCloud
 
 
 def build_wordcloud(group_to_n):
-    comment_words = []
-    for group, n in group_to_n.items():
-        if group[:4].lower() == 'the ':
-            group = group[:4]
-        group = dt.snake_to_camel(dt.to_snake(group))
-        comment_words += [group for i in range(0, n)]
-    random.shuffle(comment_words)
-    comment_words = " ".join(comment_words)
-
     wordcloud = WordCloud(
         width=1600,
         height=900,
         background_color='white',
-        stopwords=set(STOPWORDS),
-        min_font_size=10,
-    ).generate(comment_words)
+        max_words=50,
+    ).generate_from_frequencies(group_to_n)
 
     plt.figure(figsize=(16, 9), facecolor=None)
     plt.imshow(wordcloud)
