f1c2d212f428956fb1455e4f4e3056c68a63106a
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-27 16:55:52 +0530

    Added build_wordcloud_animation

--- src/news_lk2/core/news_wordcloud.py
+++ src/news_lk2/core/news_wordcloud.py
@@ -1,12 +1,15 @@
 import os
 import shutil
 
+import imageio
 import matplotlib.pyplot as plt
 from utils import timex
 from wordcloud import WordCloud
 
 from news_lk2._utils import log
 from news_lk2.core.filesys import DIR_REPO
+
+DIR_WORDCLOUDS = os.path.join(DIR_REPO, 'wordclouds')
 
 
 def build_wordcloud(group_to_n):
@@ -26,7 +29,6 @@ def build_wordcloud(group_to_n):
     plt.savefig(wordcloud_file)
     log.info(f'Saved wordcloud to {wordcloud_file}')
 
-    DIR_WORDCLOUDS = os.path.join(DIR_REPO, 'wordclouds')
     if not os.path.exists(DIR_WORDCLOUDS):
         os.mkdir(DIR_WORDCLOUDS)
 
@@ -36,3 +38,28 @@ def build_wordcloud(group_to_n):
     )
     shutil.copyfile(wordcloud_file, history_wordcloud_file)
     log.info(f'Saved wordcloud to {history_wordcloud_file}')
+
+
+def get_wordcloud_image_files():
+    image_files = []
+    for file_only in os.listdir(DIR_WORDCLOUDS):
+        image_files.append(os.path.join(DIR_WORDCLOUDS, file_only))
+    return sorted(image_files)
+
+
+def build_wordcloud_animation():
+    N_IMAGES = 10
+    image_files = get_wordcloud_image_files()
+    image_files = image_files[:N_IMAGES]
+
+    images = []
+    for image_file in image_files:
+        images.append(imageio.imread(image_file))
+
+    wordcloud_animation_file = os.path.join(DIR_REPO, 'wordcloud.gif')
+    imageio.mimsave(wordcloud_animation_file, images, duration=1)
+    log.info(f'Saved wordcloud animation to {wordcloud_animation_file}')
+
+
+if __name__ == '__main__':
+    build_wordcloud_animation()
