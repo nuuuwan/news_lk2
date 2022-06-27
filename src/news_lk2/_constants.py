371ea7386955324d01dc0ccfd3aa3f1310cd4c9c
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-27 14:34:54 +0530

    Moved readme to core

--- src/news_lk2/_constants.py
+++ src/news_lk2/_constants.py
@@ -5,3 +5,7 @@ CACHE_TIMEOUT = 3600
 TEST_MODE = False
 
 WORDS_PER_MINUTE = 250
+
+URL_GITHUB_BASE = 'https://github.com/nuuuwan/news_lk2/blob/data'
+DIR_TMP_BASE = '/tmp/news_lk2'
+DELIM_MD = '\n' * 2
