02af011ba23edc22afd5d74c7d2c6a322b84170f
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-27 21:03:11 +0530

    Added imports

--- src/news_lk2/workflows/upload_data.py
+++ src/news_lk2/workflows/upload_data.py
@@ -1,10 +1,12 @@
 from news_lk2._utils import log
 from news_lk2.core import Article
 from news_lk2.core.articles_summary import build_articles_summary
+from news_lk2.core.filesys import git_checkout
 from news_lk2.core.news_wordcloud import (build_wordcloud,
                                           build_wordcloud_animation)
 from news_lk2.core.readme import build_readme_summary
 from news_lk2.core.trends import build_trending_summary
+from news_lk2.core.upload_data import upload_data
 
 DELIM_MD = '\n' * 2
 MAX_ARTICLES_TO_UPLOAD = 200
