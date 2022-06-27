5737e6854f79d8c7e38cb0ae582fd1ab1add994a
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-27 19:28:58 +0530

    Split build_articles_summary into multiple functions

--- src/news_lk2/core/articles_summary.py
+++ src/news_lk2/core/articles_summary.py
@@ -8,31 +8,44 @@ from news_lk2.core.filesys import DIR_REPO
 N_LATEST = 400
 
 
-def build_articles_summary(articles):
-    log.info('Building articles.summary.json')
-    data_list = []
+def get_article_summary(articles):
+    articles_summary = []
     for article in articles:
-        data_list.append(
+        articles_summary.append(
             dict(
                 file_name=article.file_name,
             )
         )
+    return articles_summary
 
+
+def build_articles_summary_only(articles):
+    articles_summary = get_article_summary(articles)
     articles_summary_file = os.path.join(DIR_REPO, 'articles.summary.json')
-    JSONFile(articles_summary_file).write(data_list)
-    n_data_list = len(data_list)
-    log.info(f'Wrote {n_data_list} articles to {articles_summary_file}')
+    JSONFile(articles_summary_file).write(articles_summary)
+    n_articles_summary = len(articles_summary)
+    log.info(
+        f'Wrote {n_articles_summary} articles to {articles_summary_file}'
+    )
+    return articles_summary
 
+
+def build_articles_summary_latest(articles_summary):
     articles_summary_latest_file = os.path.join(
         DIR_REPO, 'articles.summary.latest.json'
     )
-    latest_data_list = data_list[:N_LATEST]
-    JSONFile(articles_summary_latest_file).write(latest_data_list)
-    n_latest_data_list = len(latest_data_list)
+    articles_summary_latest = articles_summary[:N_LATEST]
+    JSONFile(articles_summary_latest_file).write(articles_summary_latest)
+    n_articles_summary_latest = len(articles_summary_latest)
     log.info(
-        f'Wrote {n_latest_data_list} articles '
+        f'Wrote {n_articles_summary_latest} articles '
         + f'to {articles_summary_latest_file}',
     )
+
+
+def build_articles_summary(articles):
+    articles_summary = build_articles_summary_only(articles)
+    build_articles_summary_latest(articles_summary)
 
 
 if __name__ == '__main__':
